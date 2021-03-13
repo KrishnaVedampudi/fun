6d5aa790b7300391124aa1d10cd6930ee6888846
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2021-03-13 12:57:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+window.OneSignal = window.OneSignal || [];
+  OneSignal.push(function() {
+    OneSignal.init({
+      appId: "0727e2e0-25b1-456a-9e64-034a935c0878",
+      notifyButton: {
+        enable: true,
+      },
+    });
+  });
+  ThunkableWebviewerExtension.receiveMessage(function(message)
+    {                                
+       console.log(message);
+       fetch('https://onesignal.com/api/v1/apps/0727e2e0-25b1-456a-9e64-034a935c0878/outcomes?outcome_names=os__click.count', {
+        method: 'POST',
+        body: JSON.stringify(body),
+         headers: {               
+           "Content-type": "application/json; charset=utf-8",
+           "Authorization": "Basic NzJjNTg0NzUtMzU2Zi00OTExLTgzMTktZmJjM2Y5NDQ5Y2E4"
+         }
+    }).then(response => response.json())
+     .then(json => {
+        console.log(json);
+   });
+});
+function send()
+{                                                                                          
+              web_buttons=[
+               {
+                "id":"YES",
+                "text":"I am willing" ,
+                "url": "https://example.com/?_osp=do_not_open"
+               },
+               {
+                 "id":"NO",
+                 "text":"I am not willing",
+                 "url": "https://example.com/?_osp=do_not_open"
+               }]                 
+              const body = {    
+               "app_id": "0727e2e0-25b1-456a-9e64-034a935c0878",               
+               "contents": {"en": "Hello, someone is waiting for you!!!! If you want to aceept the job request, click on I am willing"},
+               "headings" : {"en": "A job request!"},
+               "data": {"foo":"bar"},                                          
+               "include_player_ids":[noti],             
+               "buttons": web_buttons
+              }           
+              fetch('https://onesignal.com/api/v1/notifications', {
+                     method: 'POST',
+                     body: JSON.stringify(body),
+                      headers: {               
+                        "Content-type": "application/json; charset=utf-8",
+                        "Authorization": "Basic NzJjNTg0NzUtMzU2Zi00OTExLTgzMTktZmJjM2Y5NDQ5Y2E4"
+                      }
+                 }).then(response => response.json())
+                  .then(json => {
+                     console.log(json);
+                });
+            }
