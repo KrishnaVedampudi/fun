4cd3cde295fba28d665b2ab9d844d025a8cf0fb5
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2021-03-28 18:20:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -25,14 +25,14 @@ function send()
                  "text":"I am not willing",
                  "url": "https://example.com/?_osp=do_not_open"
                }]                 
-              body = [    
+              body = [{  
                "app_id": "0727e2e0-25b1-456a-9e64-034a935c0878",               
                "contents": {"en": "Hello, someone is waiting for you!!!! If you want to aceept the job request, click on I am willing"},
                "headings" : {"en": "A job request!"},
                "data": {"foo":"bar"},                                          
                "include_player_ids":["4384c913-09f7-4b17-8344-e456ef617333"],           
                "buttons": web_buttons
-              ]      
+              }]      
               OneSignal.push(["addListenerForNotificationOpened", function(data) {
                 fetch('https://onesignal.com/api/v1/notifications', {
                      method: 'POST',
