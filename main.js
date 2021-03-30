19641394c6c961316c8e5fe0defd7e205b0c14df
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2021-03-30 18:30:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ function send()
            }).then(response => response.json())
             .then(json => {
             console.log(json); 
-           fetch('https://onesignal.com/api/v1/notifications/eb82fca8-dbb2-43db-a60e-bf7a8a550b68?app_id=0727e2e0-25b1-456a-9e64-034a935c0878&outcome_names=os__click.count&outcome_platforms=0', {
+           fetch('https://onesignal.com/api/v1/notifications/eb82fca8-dbb2-43db-a60e-bf7a8a550b68?app_id=0727e2e0-25b1-456a-9e64-034a935c0878&outcome_names=os__click.count', {
                method: 'POST',
                mode: 'no-cors',
                 headers: {               
